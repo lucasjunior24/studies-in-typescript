0ad8cff487c0c1093f897fe7f9578ee92f2e625b
unknown <lucas11souza97@gmail.com>
2023-03-27 22:37:58 -0400

    feat: created header

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,19 @@
 import { Header } from './components/Header'
-import './styles.css'
+import './global.css'
+
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
-    <Header/>
+    <div>
+      <Header/>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <aside>
+          sidebar
+        </aside>
+      </div>
+    </div>
   )
 }
