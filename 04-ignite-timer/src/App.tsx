8e70701afd7d074cb98300026840d62274bfd4bf
unknown <lucas11souza97@gmail.com>
2023-05-30 21:05:57 -0400

    feat: useding styled components

--- 04-ignite-timer/src/App.tsx
+++ 04-ignite-timer/src/App.tsx
@@ -1,6 +1,12 @@
+import { Button } from "./components/Button";
 
 export function App() {
   return (
-    <h1>Hello</h1>
-  )
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button />
+    </>
+    )
 }
--- 04-ignite-timer/src/components/Button/index.tsx
+++ 04-ignite-timer/src/components/Button/index.tsx
@@ -1,7 +1,10 @@
-import { ButtonContainer } from './styles'
+import { ButtonContainer, ButtonVariant } from './Button.styles';
 
-export function Button() {
+interface ButtonProps {
+  variant?: ButtonVariant;
+}
+export function Button({ variant = 'primary'}: ButtonProps) {
     return (
-      <ButtonContainer>Hello</ButtonContainer>
+      <ButtonContainer variant={variant}>Hello</ButtonContainer>
     )
   }
