e5a5b59139394bedee8d33cf54fbfb83af34f638
unknown <lucas11souza97@gmail.com>
2023-07-09 23:37:15 -0400

    feat: created page Home

---
+++ 04-ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,45 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountownButton,
+  TaskInput,
+} from './styled'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" placeholder="Dê um nome para seu projeto" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountownButton>
+      </form>
+    </HomeContainer>
+  )
+}
