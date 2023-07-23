b09bdeb57ed9cff286f0d14fe6df38bb6fcc1282
unknown <lucas11souza97@gmail.com>
2023-07-23 16:32:15 -0400

    feat: add formatação de data

--- 04-ignite-timer/src/pages/History/index.tsx
+++ 04-ignite-timer/src/pages/History/index.tsx
@@ -1,7 +1,8 @@
 import { useContext } from 'react'
 import { HistoryContainer, HistoryList, Status } from './styled'
 import { CycleContext } from '../../contexts/CyclesContext'
-
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 export function History() {
   const { cycles } = useContext(CycleContext)
 
@@ -23,62 +24,26 @@ export function History() {
             {cycles.map((cycle) => (
               <tr key={cycle.id}>
                 <td>{cycle.task}</td>
-                <td>20 minutos</td>
-                <td>Há 2 meses</td>
+                <td>{cycle.minutesAmount} minutos</td>
                 <td>
-                  <Status statusColor="green">Concluido</Status>
+                  {formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
+                <td>
+                  {cycle.finishedDate && (
+                    <Status statusColor="green">Concluido</Status>
+                  )}
+                  {cycle.interruptedDate && (
+                    <Status statusColor="red">Interronpido</Status>
+                  )}
+                  {!cycle.interruptedDate && !cycle.finishedDate && (
+                    <Status statusColor="yellow">Em andamento</Status>
+                  )}
                 </td>
               </tr>
             ))}
-
-            <tr>
-              <td>Dormir</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluido</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Dormir</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluido</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Dormir</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluido</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Dormir</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluido</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Dormir</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="red">Cancelado</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Dormir</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="yellow">Em andamento</Status>
-              </td>
-            </tr>
           </tbody>
         </table>
       </HistoryList>
